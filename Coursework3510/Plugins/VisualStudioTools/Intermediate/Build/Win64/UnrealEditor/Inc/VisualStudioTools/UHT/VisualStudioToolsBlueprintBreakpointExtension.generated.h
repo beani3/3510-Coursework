0a49f8f55d828cc1525027bcfa9a77f13f00d7ba
beani3 <jamestebbuft@gmail.com>
2025-11-03 10:34:02 +0000

    idk what these changes are

---
+++ Coursework3510/Plugins/VisualStudioTools/Intermediate/Build/Win64/UnrealEditor/Inc/VisualStudioTools/UHT/VisualStudioToolsBlueprintBreakpointExtension.generated.h
@@ -0,0 +1,56 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+/*===========================================================================
+	Generated code exported from UnrealHeaderTool.
+	DO NOT modify this manually! Edit the corresponding .h files instead!
+===========================================================================*/
+
+// IWYU pragma: private, include "VisualStudioToolsBlueprintBreakpointExtension.h"
+#include "UObject/ObjectMacros.h"
+#include "UObject/ScriptMacros.h"
+
+PRAGMA_DISABLE_DEPRECATION_WARNINGS
+#ifdef VISUALSTUDIOTOOLS_VisualStudioToolsBlueprintBreakpointExtension_generated_h
+#error "VisualStudioToolsBlueprintBreakpointExtension.generated.h already included, missing '#pragma once' in VisualStudioToolsBlueprintBreakpointExtension.h"
+#endif
+#define VISUALSTUDIOTOOLS_VisualStudioToolsBlueprintBreakpointExtension_generated_h
+
+#define FID_Coursework3510_Plugins_VisualStudioTools_Source_VisualStudioTools_Private_VisualStudioToolsBlueprintBreakpointExtension_h_19_INCLASS_NO_PURE_DECLS \
+private: \
+	static void StaticRegisterNativesUVisualStudioToolsBlueprintBreakpointExtension(); \
+	friend struct Z_Construct_UClass_UVisualStudioToolsBlueprintBreakpointExtension_Statics; \
+public: \
+	DECLARE_CLASS(UVisualStudioToolsBlueprintBreakpointExtension, UEditorSubsystem, COMPILED_IN_FLAGS(0), CASTCLASS_None, TEXT("/Script/VisualStudioTools"), NO_API) \
+	DECLARE_SERIALIZER(UVisualStudioToolsBlueprintBreakpointExtension)
+
+
+#define FID_Coursework3510_Plugins_VisualStudioTools_Source_VisualStudioTools_Private_VisualStudioToolsBlueprintBreakpointExtension_h_19_ENHANCED_CONSTRUCTORS \
+	/** Standard constructor, called after all reflected properties have been initialized */ \
+	NO_API UVisualStudioToolsBlueprintBreakpointExtension(); \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	UVisualStudioToolsBlueprintBreakpointExtension(UVisualStudioToolsBlueprintBreakpointExtension&&); \
+	UVisualStudioToolsBlueprintBreakpointExtension(const UVisualStudioToolsBlueprintBreakpointExtension&); \
+public: \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, UVisualStudioToolsBlueprintBreakpointExtension); \
+	DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(UVisualStudioToolsBlueprintBreakpointExtension); \
+	DEFINE_DEFAULT_CONSTRUCTOR_CALL(UVisualStudioToolsBlueprintBreakpointExtension) \
+	NO_API virtual ~UVisualStudioToolsBlueprintBreakpointExtension();
+
+
+#define FID_Coursework3510_Plugins_VisualStudioTools_Source_VisualStudioTools_Private_VisualStudioToolsBlueprintBreakpointExtension_h_16_PROLOG
+#define FID_Coursework3510_Plugins_VisualStudioTools_Source_VisualStudioTools_Private_VisualStudioToolsBlueprintBreakpointExtension_h_19_GENERATED_BODY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	FID_Coursework3510_Plugins_VisualStudioTools_Source_VisualStudioTools_Private_VisualStudioToolsBlueprintBreakpointExtension_h_19_INCLASS_NO_PURE_DECLS \
+	FID_Coursework3510_Plugins_VisualStudioTools_Source_VisualStudioTools_Private_VisualStudioToolsBlueprintBreakpointExtension_h_19_ENHANCED_CONSTRUCTORS \
+private: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+template<> VISUALSTUDIOTOOLS_API UClass* StaticClass<class UVisualStudioToolsBlueprintBreakpointExtension>();
+
+#undef CURRENT_FILE_ID
+#define CURRENT_FILE_ID FID_Coursework3510_Plugins_VisualStudioTools_Source_VisualStudioTools_Private_VisualStudioToolsBlueprintBreakpointExtension_h
+
+
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
--- Coursework3510/Source/Coursework3510/Coursework3510.Build.cs
+++ Coursework3510/Source/Coursework3510/Coursework3510.Build.cs
@@ -8,7 +8,7 @@ public class Coursework3510 : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 	
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput", "UMG" });
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput" });
 
 		PrivateDependencyModuleNames.AddRange(new string[] {  });
 
