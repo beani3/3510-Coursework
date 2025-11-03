d2e92490b1d9df7f6edbfd6a1a3cbd311f8d29fa
bbwdmu <bbwdmu@gmail.com>
2025-11-03 21:35:27 +0000

    98 per cent sure im donw with thee car skriprt
    i just need to find how to impleemnt imnnterfaces

--- Coursework3510/Source/Coursework3510/Private/MyPlayerCar.cpp
+++ Coursework3510/Source/Coursework3510/Private/MyPlayerCar.cpp
@@ -1,13 +1,82 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 #include "MyPlayerCar.h"
+
 #include "GameFramework/Controller.h"
+#include "GameFramework/PlayerController.h"
+#include "Blueprint/UserWidget.h"
+#include "Kismet/GameplayStatics.h"
+#include "Kismet/KismetSystemLibrary.h"
+
 #include "EnhancedInputComponent.h"
 #include "EnhancedInputSubsystems.h"
-#include "Blueprint/UserWidget.h"
+
+#include "ChaosVehicleMovementComponent.h"
+#include "Components/ArrowComponent.h"
+
+#include "AC_HealthComponent.h"
+#include "AC_PowerupComponentC.h"
+#include "AC_ProjectileComponent.h"
+#include "AC_PointsComponent.h"
+
+
+
+#include "GM_RaceManager.h"
+#include "ProjectileDef.h"
+
+AMyPlayerCar::AMyPlayerCar()
+{
+	// Create components
+	AC_Health = CreateDefaultSubobject<UAC_HealthComponent>(TEXT("HealthComponent"));
+	AC_PowerupComponentC = CreateDefaultSubobject<UAC_PowerupComponentC>(TEXT("PowerupComponent"));
+	AC_Projectile = CreateDefaultSubobject<UAC_ProjectileComponent>(TEXT("ProjectileComponent"));
+	AC_Points = CreateDefaultSubobject<UAC_PointsComponent>(TEXT("PointsComponent"));
+
+	//Add Interface
+
+
+	Muzzle = CreateDefaultSubobject<UArrowComponent>(TEXT("Muzzle"));
+	Muzzle->SetupAttachment(GetRootComponent()); // AWheeledVehiclePawn already has a root
+	Muzzle->SetRelativeLocation(FVector(100.f, 0.f, 50.f)); // tweak in editor as needed
+
+
+}
+
+
 
 void AMyPlayerCar::BeginPlay() {
 	Super::BeginPlay();
+
+
+	// Initialize Health
+	if (AC_Health)
+	{
+		AC_Health->InitializeHealth();
+	}
+
+	// Set Muzzle on Projectile Component
+	if (AC_Projectile && Muzzle)
+	{
+		AC_Projectile->SetMuzzle(Muzzle);
+	}
+
+
+	// Get Game Mode and cast to GM_RaceManager to bind race events
+	AGameModeBase* GameMode = UGameplayStatics::GetGameMode(this);
+	if (GameMode)
+	{
+		AGM_RaceManager* RaceManager = Cast<AGM_RaceManager>(GameMode);
+		if (RaceManager)
+		{
+			GMRaceRef = RaceManager;
+			// Bind OnStarted event
+			RaceManager->OnStarted.AddDynamic(this, &AMyPlayerCar::OnRaceStarted);
+			// Bind OnFinished event
+			RaceManager->OnFinished.AddDynamic(this, &AMyPlayerCar::OnRaceFinished);
+		}
+	}
+
+
 
 	// Input mapping context 
 	if (APlayerController* PlayerController = Cast<APlayerController>(Controller)) {
@@ -38,6 +107,10 @@ void AMyPlayerCar::SetupPlayerInputComponent(UInputComponent* PlayerInputCompone
 
 		// Pause Menu
 		EnhancedInputComponent->BindAction(PauseAction, ETriggerEvent::Triggered, this, &AMyPlayerCar::OnPauseEnter);
+
+		// powerup
+		EnhancedInputComponent->BindAction(PowerupAction, ETriggerEvent::Triggered, this, &AMyPlayerCar::UsePowerup);
+
 	}
 }
 
@@ -50,6 +123,8 @@ void AMyPlayerCar::Move(const FInputActionValue& Value) {
 		GetVehicleMovementComponent()->SetBrakeInput(MovementVector.Y * -1);
 	}
 }
+
+
 
 void AMyPlayerCar::MoveEnd() {
 	GetVehicleMovementComponent()->SetBrakeInput(0);
@@ -110,4 +185,33 @@ void AMyPlayerCar::LapCheckpoint(int32 _CheckpointNumber, int32 _MaxCheckpoints,
 	}
 
 	UE_LOG(LogTemp, Warning, TEXT("Lap: %i, Checkpoint: %i"), Lap, CurrentCheckpoint);
+}
+
+
+// Input Event: When key 'X' is pressed
+void AMyPlayerCar::UsePowerup()
+{
+	if (AC_PowerupComponentC)
+	{
+		// Activate held powerup
+		AC_PowerupComponentC->ActivateHeld();
+	}
+}
+
+
+
+
+// Race Started event handler
+void AMyPlayerCar::OnRaceStarted()
+{
+	// Print Hello string
+	UKismetSystemLibrary::PrintString(this, TEXT("Hello"), true, true, FLinearColor(0.0f, 0.66f, 1.0f, 1.0f), 2.0f);
+
+	GetVehicleMovementComponent()->SetHandbrakeInput(false);
+}
+
+// Race Finished event handler
+void AMyPlayerCar::OnRaceFinished()
+{
+	GetVehicleMovementComponent()->SetHandbrakeInput(true);
 }
--- Coursework3510/Source/Coursework3510/Public/MyPlayerCar.h
+++ Coursework3510/Source/Coursework3510/Public/MyPlayerCar.h
@@ -5,17 +5,42 @@
 #include "WheeledVehiclePawn.h"
 #include "InputActionValue.h"
 #include "ChaosVehicleMovementComponent.h"
+//#include "PowerupUserInterface.h"
+//#include "BPI_ScoreReceiver.h"
 #include "MyPlayerCar.generated.h"
 
+class UAC_HealthComponent;
+class UAC_PowerupComponentC;
+class UAC_ProjectileComponent;
+class UChaosVehicleMovementComponent;
+class UArrowComponent;
+class AGM_RaceManager; // assumed GM_RaceManager
+class ABP_RaceManager; // assumed BP_RaceManager
+class UAC_PointsComponent;
+
+
+
+
+
+
+
 UCLASS()
-class COURSEWORK3510_API AMyPlayerCar : public AWheeledVehiclePawn
+class COURSEWORK3510_API AMyPlayerCar : public AWheeledVehiclePawn/* public IPowerUpUserInterface, public IBPI_ScoreReceiver*/
 {
 	GENERATED_BODY()
+public:
+	AMyPlayerCar();
+
 public:
 	void BeginPlay();
 	virtual void SetupPlayerInputComponent(UInputComponent* PlayerInputComponent);
 
-	
+	/*virtual void ApplyBuff_Implementation(const UBuffDef* Buff) override;
+
+	virtual void FireProjectileFromDef_Implementation(const UProjectileDef* Def) override;
+
+	virtual void GivePoints_Implementation(int32 Amount, FName Reason, AActor* Causer) override;*/
+
 	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Input")
 	class UInputMappingContext* DefaultMappingContext;
 
@@ -33,6 +58,14 @@ public:
 
 	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Input")
 	class UInputAction* SteeringAction;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Input")
+	class UInputAction* PowerupAction;
+
+
+
+
+
 
 	void Move(const FInputActionValue& Value);
 	void MoveEnd();
@@ -52,6 +85,45 @@ public:
 	int32 Lap = 1;
 	int32 CurrentCheckpoint = 0;
 	void LapCheckpoint(int32 _CheckpointNumber, int32 _MaxCheckpoints, bool _bStartFinishLine);
+
+
+
+
+    UFUNCTION()
+    void OnRaceStarted();
+    UFUNCTION()
+    void OnRaceFinished();
+
+	UFUNCTION()
+	void UsePowerup();
+
+    
+
+
+
+
+	/* ===== Components ===== */
+// Created in C++ so they’re always valid
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Components")
+	UAC_HealthComponent* AC_Health = nullptr;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Components")
+	UAC_PowerupComponentC* AC_PowerupComponentC = nullptr;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Components")
+	UAC_ProjectileComponent* AC_Projectile = nullptr;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Components")
+	UAC_PointsComponent* AC_Points = nullptr;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Components")
+	UArrowComponent* Muzzle = nullptr;
+
+	/* ===== Race Manager ===== */
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Race")
+	AGM_RaceManager* GMRaceRef = nullptr;
+
+
 
 private:
 
