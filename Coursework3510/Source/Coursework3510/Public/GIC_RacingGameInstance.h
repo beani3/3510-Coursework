d202a132239585b4bbf97f6b2e3d3e0ddccdc5f8
JHalstDMU <149506457+JHalstDMU@users.noreply.github.com>
2025-11-10 21:17:00 +0000

    Set up widgets and Game Instance CPP file for ben's help later

---
+++ Coursework3510/Source/Coursework3510/Public/GIC_RacingGameInstance.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/GameInstance.h"
+#include "GIC_RacingGameInstance.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class COURSEWORK3510_API UGIC_RacingGameInstance : public UGameInstance
+{
+	GENERATED_BODY()
+	
+};
